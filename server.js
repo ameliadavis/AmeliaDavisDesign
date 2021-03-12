e183c7613d3d025a4afa804aca450acfdb23240d
Amelia Davis <ameliaaltman22@gmail.com>
2021-03-11 21:53:52 -0700

    trying to resolve routes for deploy on digital ocean

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.post('/MailDataServer', (req, res) => {
 // HTML Routes
 //  ================================================================
 app.get('/', (req, res)=>{
-  res.sendFile(path.join(__dirname,'./public', 'index.html'));
+  res.sendFile(path.join(__dirname,'/public', 'index.html'));
 })
 
 app.get("/MyWork", function(req, res) {
